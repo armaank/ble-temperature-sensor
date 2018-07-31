7d09c864e71c212b9bf47e3585dd8b550638e54e
armaan <kohli@cooper.edu>
2018-07-31 14:31:09 -0400

    debugged and improved scripts

---
+++ install.sh
@@ -0,0 +1,13 @@
+# written by armaan kohli
+  
+#!/bin/sh
+# install programs/dependencies 
+sudo apt-get update
+sudo apt-get install bluetooth -y
+sudo apt-get install bluez -y
+sudo apt-get install python-pip -y
+sudo apt-get install libglib2.0-dev -y
+pip install bluepy
+pip install schedule
+
+clear
