c55ce0b6af6276129247d9b522c6c152785a4353
armaank <kohli@cooper.edu>
2018-08-21 17:21:52 -0400

    fast sampling scripts

---
+++ scripts/fast/deliv/rcs/manage.py
@@ -0,0 +1,19 @@
+# written by armaan kohli
+# manage.py - a program used to control when ble thermistor devices are polled
+
+import os
+import sys
+import time
+import subprocess
+import datetime
+
+import schedule
+
+def run_script():
+	subprocess.call("python ~/rcs/gettemp.py", shell=True)
+
+schedule.every(30).seconds.do(run_script)
+#scheulde.every().hour.do(run_script)
+while True:
+	schedule.run_pending()
+	time.sleep(1)
