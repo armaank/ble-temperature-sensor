839aa3cee8b2bba9935a6e147546a53dca1a7e8d
armaank <kohli@cooper.edu>
2018-08-22 12:50:46 -0400

    fixed csv writing

--- scripts/slow/test/csv_write.py
+++ scripts/slow/test/csv_write.py
@@ -2,10 +2,10 @@ from time import sleep, strftime, time
 
 def csv_write(temp, name):
 	with open("./temp2.csv", "a") as log:
-		log.write("{0},{1},{2}\n".name, format(strftime("%Y-%m-%d %H:%M:%S"),str(temp),name ))
+		log.write("{0},{1},{2},{3}\n".format(strftime("%Y-%m-%d"),strftime("%H:%M:%S"),name,str(temp)))
 
 
-temp = int(input('Enter Temp Value:'))
-name = input('Enter Name:')
+temp = int(raw_input('Enter Temp Value:'))
+name = raw_input('Enter Name:')
 csv_write(temp, name)
 print('temperatures: ',temp)
